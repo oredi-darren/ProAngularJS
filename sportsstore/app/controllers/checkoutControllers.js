4145dd6607ced1c7eb478c9fc31f2bbfc36bcbf8
Darren Seet Serkai <darren@seet.me>
2014-04-24 17:59:15 +0900

    Partial development of Orders and Administration Screen

--- sportsstore/app/controllers/checkoutControllers.js
+++ sportsstore/app/controllers/checkoutControllers.js
@@ -6,7 +6,11 @@ angular.module("sportsStore")
     function($scope, cart) {
         $scope.cartData = cart.getProducts();
         $scope.total = function() {
-            return cartData.total();
+            var total = 0;
+            for (var i = 0; i < $scope.cartData.length; i++) {
+                total += ($scope.cartData[i].price * $scope.cartData[i].count);
+            }
+            return total;
         };
         $scope.remove = function(id) {
             cart.removeProduct(id);
