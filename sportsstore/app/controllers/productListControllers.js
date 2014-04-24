b069f1ff4ade22b664f0ae4da13a629669d00d75
Darren Seet Serkai <darren@seet.me>
2014-04-24 11:12:22 +0900

    Complete initial store functinos

---
+++ sportsstore/app/controllers/productListControllers.js
@@ -0,0 +1,30 @@
+/**
+ * Created by dseet on 4/24/2014.
+ */
+angular.module("sportsStore")
+    .constant("productListActiveClass", "btn-primary")
+    .constant("productListCount", 3)
+    .controller("productListCtrl",
+        function($scope, $filter, productListActiveClass, productListCount) {
+            var selectedCategory = null;
+            $scope.selectedPage = 1;
+            $scope.pageSize = productListCount;
+
+            $scope.selectCategory = function(newCategory) {
+                selectedCategory = newCategory;
+                $scope.selectedPage = 1;
+            };
+            $scope.selectPage = function(newPage) {
+                $scope.selectedPage = newPage;
+            };
+            $scope.categoryFilterFn = function(product) {
+                return (selectedCategory == null) || (product.category == selectedCategory);
+            };
+            $scope.getCategoryClass = function(category) {
+                return (selectedCategory == category) ? productListActiveClass : "";
+            };
+            $scope.getPageClass = function(page) {
+                return ($scope.selectedPage == page) ? productListActiveClass : "";
+            };
+        }
+);
