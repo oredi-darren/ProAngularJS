9da5bfff6c07dcd5974654e5a09d0b5b6090df0a
oredi <darren_seet@oredi.com>
2014-04-27 10:11:02 +0900

    Added new admin partials

--- sportsstore/app/controllers/adminControllers.js
+++ sportsstore/app/controllers/adminControllers.js
@@ -3,6 +3,7 @@
  */
 angular.module("sportsStoreAdmin")
     .constant("authUrl", "http://localhost:5500/users/login")
+    .constant("ordersUrl", "http://localhost:5500/orders")
     .controller("authCtrl", function($scope, $http, $location, authUrl) {
         $scope.authenticate = function(user, pass) {
             $http.post(authUrl, {
@@ -16,4 +17,24 @@ angular.module("sportsStoreAdmin")
                 $scope.authenticationError = error;
             });
         }
+    })
+    .controller("mainCtrl", function($scope) {
+        $scope.screens = ["Products", "Orders"];
+        $scope.current = $scope.screens[0];
+        $scope.setScreen = function(index) {
+            $scope.current = $scope.screens[index];
+        };
+        $scope.getScreen = function(index) {
+            return $scope.current == "Products" ? "views/adminProducts.html" : "views/adminOrders.html";
+        };
+    })
+    .controller("ordersCtrl", function($scope, $http, ordersUrl) {
+        $http.get(ordersUrl, { withCredentials: true})
+            .success(function (data) {
+                $scope.orders = data;
+            })
+            .error(function(error) {
+                $scope.error = error;
+            });
+
     });
