d489a98f9a1e59d5d97b9685053bd1f93ce3efd6
Darren Seet Serkai <darren@seet.me>
2014-05-13 09:31:54 +0900

    Chapter 25 Unit Testing, completed listing 9

--- chapter25/app.js
+++ chapter25/app.js
@@ -2,13 +2,23 @@
  * Created by dseet on 5/13/2014.
  */
 angular.module("exampleApp", [])
-    .controller("defaultCtrl", function ($scope, $http) {
-        $scope.counter = 0;
+    .controller("defaultCtrl", function ($scope, $http, $interval, $timeout) {
+        $scope.intervalCounter = 0;
+        $scope.timerCounter = 0;
+
+        $interval(function() {
+            $scope.intervalCounter++;
+        }, 5000, 10);
+
+        $timeout(function() {
+            $scope.timerCounter++;
+        }, 5000);
 
         $http.get("productData.json").success(function (data) {
             $scope.products = data;
         });
 
+        $scope.counter = 0;
         $scope.incrementCounter = function() {
             $scope.counter++;
         };
