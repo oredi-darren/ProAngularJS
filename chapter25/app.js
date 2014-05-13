fb4b2b9d7abce1c4054e80c0e4e189ae5ff59edd
Darren Seet Serkai <darren@seet.me>
2014-05-13 09:24:55 +0900

    Chapter 25 Unit Testing, completed listing 7

--- chapter25/app.js
+++ chapter25/app.js
@@ -2,8 +2,12 @@
  * Created by dseet on 5/13/2014.
  */
 angular.module("exampleApp", [])
-    .controller("defaultCtrl", function ($scope) {
+    .controller("defaultCtrl", function ($scope, $http) {
         $scope.counter = 0;
+
+        $http.get("productData.json").success(function (data) {
+            $scope.products = data;
+        });
 
         $scope.incrementCounter = function() {
             $scope.counter++;
--- chapter25/tests/controllerTest.js
+++ chapter25/tests/controllerTest.js
@@ -2,15 +2,25 @@
  * Created by dseet on 5/13/2014.
  */
 describe("Controller Test", function () {
-    // Arange
-    var mockScope = {};
-    var controller;
+    // Arrange
+    var mockScope, controller, backend;
     beforeEach(angular.mock.module("exampleApp"));
-    beforeEach(angular.mock.inject(function ($controller, $rootScope) {
+    beforeEach(angular.mock.inject(function ($httpBackend) {
+        backend = $httpBackend;
+        backend.expect("GET", "productData.json")
+            .respond([
+                    { "name": "Apples", "category": "Fruit", "price": 1.20 },
+                    { "name": "Bananas", "category": "Fruit", "price": 2.42 },
+                    { "name": "Pears", "category": "Fruit", "price": 2.02 }]);
+    }));
+
+    beforeEach(angular.mock.inject(function ($controller, $rootScope, $http) {
         mockScope = $rootScope.$new();
         controller = $controller("defaultCtrl", {
-            $scope: mockScope
+            $scope: mockScope,
+            $http: $http
         });
+        backend.flush();
     }));
 
     // Act and Assess
@@ -21,5 +31,20 @@ describe("Controller Test", function () {
     it("Increments counter", function () {
         mockScope.incrementCounter();
         expect(mockScope.counter).toEqual(1);
+    });
+
+    it("Makes an Ajax request", function () {
+        backend.verifyNoOutstandingExpectation();
+    });
+
+    it("Process the data", function () {
+        expect(mockScope.products).toBeDefined();
+        expect(mockScope.products.length).toEqual(3);
+    });
+
+    it("Preserves the data order", function () {
+        expect(mockScope.products[0].name).toEqual("Apples");
+        expect(mockScope.products[1].name).toEqual("Bananas");
+        expect(mockScope.products[2].name).toEqual("Pears");
     });
 });
